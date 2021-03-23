3112efef2730029d2064dde8d71b3b2549aa819a
Nerijus Latakas <nerijus.latakas@gmail.com>
2021-03-23 12:49:30 +0200

    dovydazz ar ok ar nori su visu requestu?

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # coordinates
 calculate nearest coordinate from aray given
 
+Make jquery post to coordinates.php with array of coordinates. 
+
+Array ecample: 
+
+```
+$locations = array (
+ array (
+  "postcode" => "2850",
+  "name" => "Aarons Pass",
+  "state_code" => "NSW",
+  "lat" => "-32.86328",
+  "lng" => "149.80375"
+ ),
+ array (
+  "postcode" => "6280",
+  "name" => "Abba River",
+  "state_code" => "WA",
+  "lat" => "-33.68488",
+  "lng" => "115.46334"
+ ),
+ array (
+  "postcode" => "6280",
+  "name" => "Abbey",
+  "state_code" => "WA",
+  "lat" => "-33.66077",
+  "lng" => "115.25863"
+ )
+);
+```
+
 Don't forget to add HOME coordinates in coordinates.php
 ```
 /*
---
+++ coordinates.php
@@ -0,0 +1,37 @@
+<?php
+/*
+* set Your HOME location
+*/
+$base_location = array(
+  'lat' => "-31.959138",
+  'lng' => "115.858072"
+);
+
+//get locations array from post
+if (isset($_POST['locations']))
+//if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['locations']))
+{
+
+    $locations = $_POST['locations'];
+    
+    // calculate
+
+    //distances array from POSTED data
+    $distances = array();
+
+    foreach ($locations as $key => $location)
+    {
+        $delta_lat = $base_location['lat'] - $location['lat'];
+        $delta_lng = $base_location['lng'] - $location['lng'];
+        $mean_lat = ($location['lat'] + $base_location['lat']) / 2;
+        $distance = ($delta_lat**2) + ((cos($mean_lat) * $delta_lng)**2);
+
+        $earth_radius_km = 6371.009;
+        $actual_distance = $earth_radius_km * sqrt($distance);
+        $distances[$key] = $actual_distance;
+    }
+
+    $closest = $locations[key($distances)];
+    echo $output;
+    //return $closest;
+}    
